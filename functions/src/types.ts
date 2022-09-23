d8cca8f4653fcf47733f27f4ff9699c1ebb4a874
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2022-09-23 18:35:14 -0400

    style: 🎨 preittier

--- functions/src/types.ts
+++ functions/src/types.ts
@@ -1,15 +1,8 @@
-import * as admin from 'firebase-admin';
+import * as admin from "firebase-admin";
 
-export const colors: Color[] = [
-  'red',
-  'blue',
-  'green',
-  'orange',
-  'purple',
-  'yellow'
-];
+export const colors: Color[] = ["red", "blue", "green", "orange", "purple", "yellow"];
 
-export type Color = 'red' | 'orange' | 'yellow' | 'green' | 'blue' | 'purple';
+export type Color = "red" | "orange" | "yellow" | "green" | "blue" | "purple";
 
 export interface Game {
   board: Cell[];
@@ -24,16 +17,16 @@ export type Player = {
   color: Color;
   nickname: string;
   points: number;
-}
+};
 
 export type Cell = {
   owner: number;
   tower: boolean;
-}
+};
 
 export type Move = {
   positions: number[];
-}
+};
 
 export function isValidMove(move: any, game: Game): move is Move {
   if (game.winner !== -1) {
@@ -65,7 +58,7 @@ export function getArrayLength(sideLength: number) {
   return 3 * (sideLength - 1) * sideLength + 1;
 }
 
-export type Direction = 'UR' | 'UL' | 'NR' | 'NL' | 'DR' | 'DL';
+export type Direction = "UR" | "UL" | "NR" | "NL" | "DR" | "DL";
 
 export class GridData {
   rowStartsMap: { [key: number]: number[] } = {};
@@ -75,7 +68,7 @@ export class GridData {
       let id = 0;
       this.rowStartsMap[sideLength] = new Array(sideLength * 2).fill(null).map((_value, row) => {
         if (row > 0) {
-          id += (sideLength * 2 - 1) - Math.abs((row - 1) - (sideLength - 1));
+          id += sideLength * 2 - 1 - Math.abs(row - 1 - (sideLength - 1));
         }
         return id;
       });
@@ -91,30 +84,36 @@ export class GridData {
     const RowStarts = this.getRowStarts(sideLength);
     let currentRow = 0;
     while (RowStarts[currentRow + 1] <= current) currentRow++;
-    return (directions || ['UR', 'UL', 'NR', 'NL', 'DR', 'DL']).map(direction => {
+    return (directions || ["UR", "UL", "NR", "NL", "DR", "DL"]).map((direction) => {
       switch (direction) {
-        case 'NR':
-          return (RowStarts[currentRow + 1] - 1 === current) ? -1 : current + 1;
-        case 'NL':
-          return (RowStarts[currentRow] === current) ? -1 : current - 1;
-        case 'UR':
-        case 'UL':
-        case 'DR':
-        case 'DL':
+        case "NR":
+          return RowStarts[currentRow + 1] - 1 === current ? -1 : current + 1;
+        case "NL":
+          return RowStarts[currentRow] === current ? -1 : current - 1;
+        case "UR":
+        case "UL":
+        case "DR":
+        case "DL":
           const rowPosition = current - RowStarts[currentRow];
-          const left = direction.charAt(1) === 'L';
+          const left = direction.charAt(1) === "L";
           switch (direction) {
-            case 'UR':
-            case 'UL':
-              const above = (currentRow - (sideLength - 1) <= 0);
-              if (currentRow === 0 || (above && (left ? RowStarts[currentRow] === current : RowStarts[currentRow + 1] - 1 === current))) {
+            case "UR":
+            case "UL":
+              const above = currentRow - (sideLength - 1) <= 0;
+              if (
+                currentRow === 0 ||
+                (above && (left ? RowStarts[currentRow] === current : RowStarts[currentRow + 1] - 1 === current))
+              ) {
                 return -1;
               }
               return RowStarts[currentRow - 1] + (left ? -0.5 : +0.5) + (above ? -0.5 : 0.5) + rowPosition;
-            case 'DR':
-            case 'DL':
-              const below = (currentRow - (sideLength - 1) >= 0);
-              if (currentRow === sideLength * 2 - 2 || (below && (left ? RowStarts[currentRow] === current : RowStarts[currentRow + 1] - 1 === current))) {
+            case "DR":
+            case "DL":
+              const below = currentRow - (sideLength - 1) >= 0;
+              if (
+                currentRow === sideLength * 2 - 2 ||
+                (below && (left ? RowStarts[currentRow] === current : RowStarts[currentRow + 1] - 1 === current))
+              ) {
                 return -1;
               }
               return RowStarts[currentRow + 1] + (left ? -0.5 : +0.5) + (below ? -0.5 : 0.5) + rowPosition;
@@ -127,7 +126,11 @@ export class GridData {
 export type UserData = {
   color?: Color;
   nickname: string;
-}
+};
 
 export const nicknameMaxLength = 15;
 export const nicknameMinLength = 2;
+
+export type Queue = {
+  [uid: string]: true;
+};
